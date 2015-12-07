edded6dbeb1e7932c96e71f59bf7bcd727d75172
Serge Dukic <serge@clearpoint.co.nz>
2015-12-08 00:22:49 +1300

    Completed Perl equivalent
    Made the Perl code do the same thing as the Java code. Was much more pleasant. Particularly the use of "bigint" at the
    top meaning that all Integer types are automatically overloaded.
    
    Also, note the lack of code needed to manipulate types.
    
    Issue(s): None

--- v2/pubkey.pl
+++ v2/pubkey.pl
@@ -1,10 +1,37 @@
 #!/usr/bin/perl
 
-$p = 11;
-$q = 13;
+use bigint;
+
+$p = 13;
+$q = 17;
 $exponent = 7;
 $function = ($p - 1) * ($q - 1);
 print "P & Q: " . $p . ", " . $q . "\n";
 print "Modulus: " . ($p * $q) . "\n";
 print "Exponent: " . $exponent . "\n";
 print "Public key (encryption exponent, modulus) is: (" . $exponent . ", " . ($p * $q) . ")\n";
+
+# Calculate the decryption (private) key
+$privateDecryptKey = 0;
+for($i = 0; $i < 1000; $i++){
+  if(($i * $exponent) % $function == 1){
+    $privateDecryptKey = $i;
+    break;
+  }
+}
+print "Private key is: " . $privateDecryptKey . "\n";
+
+$secretMessage = "Hello World!";
+print "Secret message is: " . $secretMessage . "\n";
+
+# Encrypt the message
+foreach(split //, $secretMessage){
+  print ord($_)**$exponent % ($p * $q) . "\n";
+  push @encryptedChars, (ord($_)**$exponent % ($p * $q));
+}
+
+# Decrypt the message with the private key
+foreach (@encryptedChars){
+  print chr(($_**$privateDecryptKey % ($p * $q)));
+}
+print "\n";
